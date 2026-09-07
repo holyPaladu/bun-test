72bba8343f2197d43ffede9078a59430c618a8ad
holyPaladu <ironicpaladin2907@gmail.com>
2026-09-07 15:07:44 +0500

    upd (types) : for meta and result

--- services/auth-service/src/shared/types/meta.type.ts
+++ services/auth-service/src/shared/types/meta.type.ts
@@ -1,4 +1,13 @@
-export interface meta {
+export interface Meta {
   ip: string | null
   userAgent: string | null
+}
+
+export type Pagination = {
+  page: number
+  perPage: number
+}
+export type PaginationInside = {
+  limit: number
+  offset: number
 }
